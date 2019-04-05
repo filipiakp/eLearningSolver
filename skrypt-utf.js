47e70ae6151fa24b3aa1b1c3622ca6d66a5fc683
Paweł Filipiak <g12pawelfilipiak@gmail.com>
2019-04-05 09:43:26 +0200

    Version 2
    Corrected regex and names of variables are English now

--- skrypt-utf.js
+++ skrypt-utf.js
@@ -1,39 +1,40 @@
-﻿var regex = /\*[ \/\\\(\)\_\dA-Za-z\&;.\<\>]+\*/g;
-var s = -1;
-var saOdpowiedzi = false;
+﻿var regex = /\*[\/\\\(\)\_\dA-Za-z\&;.\<\>]+[ \/\\\(\)\_\dA-Za-z\&;,.\<\>]{0,}(: [a-z]+\?)?\*/g;
+var s = -1;//script counter
+var isH5PScript = false;
 do{
 	if(typeof document.scripts[++s] !== "undefined"){
-		saOdpowiedzi = document.scripts[s].innerHTML.includes("var H5PIntegration")? true:false;
+		isH5PScript = document.scripts[s].innerHTML.includes("var H5PIntegration")? true:false;
 	}
-}while(!saOdpowiedzi && s<10);
+}while(!isH5PScript && s<10);
 
-odpowiedzi = Array.from(document.scripts[s].innerHTML.matchAll(regex));
+answers = Array.from(document.scripts[s].innerHTML.matchAll(regex));//answers array with *, HTML entities and unicode
 
-odp_tab = new Array();
-for(var i=0; i<odpowiedzi.length; i++){
-	odp = odpowiedzi[i][0].toString();
-	odp = odp.replace(/&lt;/g,"<");
-	odp = odp.replace(/&gt;/g,">");
-	odp = odp.replace(/&amp;/g,"&");
-	odp = odp.replace(/&ast;/g,"*");
-	odp = odp.replace(/\*/g,"");
-	odp = odp.replace(/\\\\/g,"\\");
-	odp = JSON.parse("\""+odp+"\"");
-	odp = odp.match(/\\/)?odp.substring(0,odp.indexOf("\\")):odp;
-	odp_tab[i] = odp;
+answersArray = new Array();//array for ready-to-insert answers
+for(var i=0; i<answers.length; i++){
+	answer = answers[i][0].toString();
+	answer = answer.replace(/&lt;/g,"<");
+	answer = answer.replace(/&gt;/g,">");
+	answer = answer.replace(/&amp;/g,"&");
+	answer = answer.replace(/&ast;/g,"*");
+	answer = answer.replace(/(: [a-z]+\?)/g,"");
+	answer = answer.replace(/\*/g,"");
+	answer = answer.replace(/\\\\/g,"\\");
+	answer = JSON.parse("\""+answer+"\"");//decodes unicode
+	answer = answer.match(/\\/)?answer.substring(0,answer.indexOf("\\")):answer;//cuts other good answers
+	answersArray[i] = answer;
 }
 
 h5pdocument = document.getElementsByClassName("h5p-iframe-wrapper")[0].childNodes[0].contentWindow.document;
-slajdy = h5pdocument.getElementsByClassName("h5p-progressbar-part");
-var slajd = 0; 
+slides = h5pdocument.getElementsByClassName("h5p-progressbar-part");
+var slide = 0; 
 var j = 0;
-while(j<odp_tab.length){
-	inputy = h5pdocument.getElementsByClassName("h5p-text-input");
-	for(var k=0;k<inputy.length;k++){
-		inputy[k].value = odp_tab[j];
-		inputy[k].click();
+while(j<answersArray.length){
+	inputs = h5pdocument.getElementsByClassName("h5p-text-input");
+	for(var k=0;k<inputs.length;k++){
+		inputs[k].value = answersArray[j];
+		inputs[k].click();
 		j++;
 	}
-	slajdy[++slajd].childNodes[0].click();
+	slides[++slide].childNodes[0].click();
 }
 
